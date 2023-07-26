66518461280af07186565351e4cf91923f980574
FredericoBresani <fbs4@cin.ufpe.br>
2023-07-26 12:09:26 -0300

    testing code

--- program.hs
+++ program.hs
@@ -1,37 +1,61 @@
-{- #########################################################
+dobro :: Integer -> Integer
+dobro x = 2*x
 
-        FirstScript.hs
-        Simon Thompson, August 2010.
+quadruplo :: Integer -> Integer
+quadruplo x = 2*dobro(x)
 
-######################################################### -}
+poli2 :: Double -> Double -> Double -> Double -> Double
+poli2 a b c x = a*x*x + b*x + c
 
-module FirstScript where
 
---      The value size is an integer (Integer), defined to be 
---      the sum of twelve and thirteen.
+parImpar :: Int -> String
+parImpar x
+        | mod x 2 == 0 = "Par"
+        | otherwise = "Impar"
 
-size :: Integer
-size = 12+13
+maxThree :: Int -> Int -> Int -> Int
+maxThree x y z = max (max x y) z
 
---      The function to square an integer.
+maxFour :: Int -> Int -> Int -> Int -> Int
+maxFour x y z w = max w (maxThree x y z)
 
-square :: Integer -> Integer
-square n = n*n
+quantosIguais :: Int -> Int -> Int -> Int
+quantosIguais x y z
+        | x == y && y == z = 3
+        | x == y = 2
+        | y == z = 2
+        | otherwise = 0
 
---      The function to double an integer.
+ehZero :: Int -> Bool
+ehZero 0 = True
+ehZero _ = False
+
+sumTo :: Integer -> Integer
+sumTo 1 = 1
+sumTo n = n + sumTo(n-1)
+
+potencia :: Integer -> Integer -> Integer
+potencia n 1 = n
+potencia n k = n*(potencia n (k-1)) 
+
+binomiais :: Integer -> Integer -> Integer
+binomiais n 0 = 1
+binomiais 0 k = maiorQue k
+        where 
+        maiorQue :: Integer -> Integer
+        maiorQue k
+                | k > 0 = 0
+                | otherwise = k
+binomiais n k = binomiais (n-1) k + binomiais (n-1) (k-1)
+
+fib :: Int -> (Int, Int) 
+fib 0 = (0, 0)
+fib 1 = (0, 1)
+fib n = ()
         
-double :: Integer -> Integer
-double n = 2*n
 
---      An example using double, square and size.
-         
-example :: Integer
-example = double (size - square (2+2))
-
-doubleSquare :: Integer -> Integer
-doubleSquare x = double(square x)
-
-charToNum :: Char -> Int
-charToNum c
-    | fromEnum (c) > 57 || fromEnum(c) < 48 = 0
-    | otherwise = fromEnum(c) - 48
+{-
+fib(4) + fib(3)
+fib(3) + fib(2) + fib(2) + fib(1)
+fib(2) + fib(1) + fib(2) + fib(2) + fib(1)
+-}
